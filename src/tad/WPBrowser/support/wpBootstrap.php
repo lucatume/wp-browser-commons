14d8bf602e8a8d825acb5d7cc656f0e003190ea5
Luca Tumedei <luca@theaveragedev.com>
2016-06-09 09:31:17 +0200

    code format

--- src/tad/WPBrowser/support/wpBootstrap.php
+++ src/tad/WPBrowser/support/wpBootstrap.php
@@ -6,16 +6,16 @@
 // Include wp-load.php to bootstrap WordPress 
 include $argv[1];
 
-$request = unserialize( $argv[2] );
+$request = unserialize($argv[2]);
 
 // signon the user; will set the cookies but on next page reload...
-$user = wp_signon( $request['credentials'] );
+$user = wp_signon($request['credentials']);
 
 //... so let's set and use the ones we've been passed.
-$_COOKIE = array_merge( $_COOKIE, $request['cookies'] );
+$_COOKIE = array_merge($_COOKIE, $request['cookies']);
 
-wp_set_current_user( $user->ID, $user->user_login );
+wp_set_current_user($user->ID, $user->user_login);
 
-$nonce = wp_create_nonce( $request['action'] );
+$nonce = wp_create_nonce($request['action']);
 
-die ( $nonce );
+die ($nonce);
