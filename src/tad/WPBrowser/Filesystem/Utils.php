aec9b442bdf80cfd1be5bf3e9e8a3bd595b5dc00
Luca Tumedei <luca@theaveragedev.com>
2016-10-31 16:54:15 +0100

    add recursive copy method:

--- src/tad/WPBrowser/Filesystem/Utils.php
+++ src/tad/WPBrowser/Filesystem/Utils.php
@@ -6,8 +6,8 @@ class Utils
 {
 
     public static function findHereOrInParent($frag,
-        $start,
-        Filesystem $filesystem = null)
+                                              $start,
+                                              Filesystem $filesystem = null)
     {
         if (!is_string($frag)) {
             throw new  \InvalidArgumentException('Frag must be a string');
@@ -42,7 +42,7 @@ class Utils
      * @return string The path with the `~` replaced with the user home path if any.
      */
     public static function homeify($path,
-        Filesystem $filesystem = null)
+                                   Filesystem $filesystem = null)
     {
         if (!is_string($path)) {
             throw new \InvalidArgumentException('Paht must be a string');
@@ -71,5 +71,53 @@ class Utils
             throw new \InvalidArgumentException('Path must be a string');
         }
         return $path !== DIRECTORY_SEPARATOR ? ltrim($path, DIRECTORY_SEPARATOR) : $path;
+    }
+
+    /**
+     * Recursively copies a source to a destination.
+     *
+     * @param string $source The absolute path to the source.
+     * @param string $destination The absolute path to the destination.
+     */
+    public static function recurseCopy($source, $destination)
+    {
+        $dir = opendir($source);
+        @mkdir($destination);
+        while (false !== ($file = readdir($dir))) {
+            if (($file != '.') && ($file != '..')) {
+                if (is_dir($source . '/' . $file)) {
+                    static::recurseCopy($source . '/' . $file, $destination . '/' . $file);
+                } else {
+                    copy($source . '/' . $file, $destination . '/' . $file);
+                }
+            }
+        }
+        closedir($dir);
+    }
+
+    /**
+     * Recursively deletes a target directory.
+     *
+     * @param string $target The absolute path to a directory to remove.
+     */
+    public static function recurseRemoveDir($target)
+    {
+        if (!file_exists($target)) {
+            return;
+        }
+
+        $dir = opendir($target);
+        while (false !== ($file = readdir($dir))) {
+            if (($file != '.') && ($file != '..')) {
+                $full = $target . '/' . $file;
+                if (is_dir($full)) {
+                    static::recurseRemoveDir($full);
+                } else {
+                    unlink($full);
+                }
+            }
+        }
+        closedir($dir);
+        rmdir($target);
     }
 }
